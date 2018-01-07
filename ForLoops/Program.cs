d8e073c8a1a1df141d1dd34b72d90d095d323c7e
LSLIM <LSLIM@LS>
2018-01-07 18:43:43 +0100

    Delete wrong syntax. It should work now

--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -6,7 +6,11 @@ namespace ForLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
